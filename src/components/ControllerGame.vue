80e130d44c7f36b08610bc853c67c30f94b0c4b9
Victor Prata <victor.prata@buser.com.br>
2023-01-05 10:28:15 -0300

    Troca caractere por emojis no mock

---
+++ src/components/ControllerGame.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="box">
+    <div class="field has-addons has-addons-centered is-marginless">
+      <p class="control">
+        <button
+          :disabled="mainComponent !== 'gamePage'"
+          :class="isRunning ? 'green' : 'red'"
+          class="button"
+          title="play"
+          @click="send('play')"
+        >
+          <v-icon v-show="!isRunning">mdi-play</v-icon>
+          <v-icon v-show="isRunning">mdi-pause</v-icon>
+        </button>
+      </p>
+      <p class="control">
+        <button
+          :disabled="mainComponent !== 'gamePage'"
+          class="button"
+          title="clear grid"
+          @click="send('redoSession')"
+        >
+          <v-icon> mdi-restart</v-icon>
+        </button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isRunning: {
+      default: false,
+      type: Boolean,
+    },
+    mainComponent: {
+      default: "gamePage",
+      type: String,
+    },
+  },
+  methods: {
+    /**
+     * emits the action that is requested
+     * by the client up to the App component.
+     *
+     * @param {string} event - the event
+     */
+    send: function (event) {
+      this.$emit("send", event);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  padding: 0;
+}
+.green {
+  background-color: hsla(120, 77%, 26%, 0.6);
+}
+.red {
+  background-color: hsla(0, 67%, 31%, 0.6);
+}
+</style>
--- the_game_mock.py
+++ the_game_mock.py
@@ -11,7 +11,7 @@ def cria_matriz(n_linhas, n_colunas):
     for i in range(0, n_linhas):
         matriz.append([])
         for j in range(0, n_colunas):
-            matriz[i].append(0)
+            matriz[i].append(celula_morta)
 
 
 def continuar_o_jogo(continuar, pos_linha=0, pos_coluna=0):
@@ -32,7 +32,7 @@ def continuar_o_jogo(continuar, pos_linha=0, pos_coluna=0):
 def insere_celula_viva(array, i_row, j_column):
     i_row -= 1
     j_column -= 1
-    array[i_row][j_column] = '#'
+    array[i_row][j_column] = celula_viva
 
 def zera_index_negativo(index):
     if index < 0:
@@ -56,11 +56,11 @@ def contador_de_vizinhos(par_i, par_j):
     for i in range(i_inicial, i_final):
         for j in range(j_inicial, j_final):
             try:
-                if matriz[i][j] == '#':
+                if matriz[i][j] == celula_viva:
                     cont_vizinhos += 1
             except IndexError:
                 cont_vizinhos += 0
-    if matriz[par_i][par_j] == '#':
+    if matriz[par_i][par_j] == celula_viva:
         cont_vizinhos -= 1 # programa considera o próprio elemento
     if cont_vizinhos == -1:
         cont_vizinhos = 0
@@ -73,33 +73,33 @@ def inverter_caractere(list_of_lists):
         index_j = i[1]
 
         elemento = matriz[index_i][index_j]
-        if elemento == 0:
-            matriz[index_i][index_j] = '#'
+        if elemento == celula_morta:
+            matriz[index_i][index_j] = celula_viva
         else:
-            matriz[index_i][index_j] = 0
+            matriz[index_i][index_j] = celula_morta
 
 
 def roda_o_game(matriz):
     n_vizinhos = 0
-
     # TEM QUE EXCLUIR A BEIRADAS
     for i in range(0, rows):
         for j in range(0, columns):
             el = matriz[i][j]
             n_vizinhos = contador_de_vizinhos(i, j)
             if n_vizinhos <= 1 or n_vizinhos >= 4:
-                if el == '#':
+                if el == celula_viva:
                     posicoes_para_inverter.append([i, j])
             else:
-                if n_vizinhos == 3 and el == 0:
+                if n_vizinhos == 3 and el == celula_morta:
                     posicoes_para_inverter.append([i, j])
 
     inverter_caractere(posicoes_para_inverter)
-    print_matriz(matriz)
+   
 
 
 # --------------- FIM DAS FUNÇÕES
-
+celula_viva = '\U0001F7E8'
+celula_morta = '\U0001F7EA'
 matriz = []
 matriz_teste = []
 linha = []
@@ -110,21 +110,29 @@ rows = 3
 columns = 3
 
 cria_matriz(3, 3)
-
+print('-- MATRIZ INICIAL --')
 print_matriz(matriz)
+print('-- FIM MATRIZ INICIAL --') 
 
-insere_celula_viva(matriz, 1, 1)
-insere_celula_viva(matriz, 1, 2)
-insere_celula_viva(matriz, 1, 3)
 insere_celula_viva(matriz, 2, 1)
 insere_celula_viva(matriz, 2, 2)
 insere_celula_viva(matriz, 2, 3)
 
+
 print('----- INICIEI ------')
 print_matriz(matriz)
-repetidor = 'ligado'
-
-while repetidor:
+print("""----- VOU RODAR O GAME ------
+.
+.
+.
+.
+""") 
+numero = 0
+while True:
+    numero += 1
+    print(f'\n{numero}ª vez:')
     roda_o_game(matriz)
+    roda_o_game(matriz)
+    print_matriz(matriz)
     time.sleep(2)
 
